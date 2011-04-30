7461d0e465bd20bd22114dfb399bdaf80bae0fbe
brent.bb <brent.bb@gmail.com>
2011-04-29 21:36:05 -0500

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from distutils.core import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "reposado",
+    version = "git",
+    author = "Greg Neagle",
+    author_email = "reposado@googlegroups.com",
+    maintainer = "Brent B",
+    maintainer_email = "brent.bb+py@gmail.com",
+    description = ("Host Apple Software Updates on the hardware and OS of your choice."),
+    license = "BSD",
+    keywords = "apple software update repository",
+    url = "https://github.com/wdas/reposado",
+    packages=['reposadolib'],
+    package_dir={'reposadolib': 'code/reposadolib'},
+    scripts=["code/repo_sync","code/repoutil"],
+    long_description=read('README.md'),
+    classifiers=[
+    	"Intended Audience :: System Administrators",
+        "Development Status :: 1 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: System :: Archiving :: Mirroring",
+        "Topic :: System :: Installation/Setup",
+    ],
+)
