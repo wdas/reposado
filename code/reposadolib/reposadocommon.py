e10abdc52cf5a967b4d2397297ebb4c653d126ac
Greg Neagle <gregneagle@mac.com>
2013-10-07 16:22:58 -0700

    PyLint code cleanup, add sucatalog for upcoming OS X release

--- code/reposadolib/reposadocommon.py
+++ code/reposadolib/reposadocommon.py
@@ -49,10 +49,12 @@ import warnings
 from xml.parsers.expat import ExpatError
 
 def get_main_dir():
-    '''Returns the directory name of the script or the directory name of the exe if py2exe was used
+    '''Returns the directory name of the script or the directory name of the exe
+    if py2exe was used
     Code from http://www.py2exe.org/index.cgi/HowToDetermineIfRunningFromExe
     '''
-    if (hasattr(sys, "frozen") or hasattr(sys, "importers") or imp.is_frozen("__main__")):
+    if (hasattr(sys, "frozen") or hasattr(sys, "importers")
+        or imp.is_frozen("__main__")):
         return os.path.dirname(sys.executable)
     return os.path.dirname(sys.argv[0])
 
@@ -64,12 +66,22 @@ def prefsFilePath():
 def pref(prefname):
     '''Returns a preference.'''
     default_prefs = {
-        'AppleCatalogURLs':                    ['http://swscan.apple.com/content/catalogs/index.sucatalog',
-'http://swscan.apple.com/content/catalogs/index-1.sucatalog',
-'http://swscan.apple.com/content/catalogs/others/index-leopard.merged-1.sucatalog',
-'http://swscan.apple.com/content/catalogs/others/index-leopard-snowleopard.merged-1.sucatalog',
-'http://swscan.apple.com/content/catalogs/others/index-lion-snowleopard-leopard.merged-1.sucatalog',
-'http://swscan.apple.com/content/catalogs/others/index-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'],
+        'AppleCatalogURLs': [
+            ('http://swscan.apple.com/content/catalogs/'
+             'index.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/'
+             'index-1.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/others/'
+             'index-leopard.merged-1.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/others/'
+             'index-leopard-snowleopard.merged-1.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/others/'
+             'index-lion-snowleopard-leopard.merged-1.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/others/'
+             'index-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/others/'
+          'index-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog')
+        ],
         'PreferredLocalizations': ['English', 'en'],
         'CurlPath': '/usr/bin/curl'
     }
@@ -83,8 +95,8 @@ def pref(prefname):
         return default_prefs[prefname]
     else:
         return None
-        
-        
+
+
 def validPreferences():
     '''Validates our preferences to make sure needed values are defined
     and paths exist. Returns boolean.'''
@@ -96,22 +108,24 @@ def validPreferences():
                             (pref_name, prefsFilePath()))
             prefs_valid = False
         elif not os.path.exists(preference):
-             print_stderr('WARNING: %s "%s" does not exist.'
-                          ' Will attempt to create it.' %
-                          (pref_name, preference))
+            print_stderr('WARNING: %s "%s" does not exist.'
+                         ' Will attempt to create it.' %
+                         (pref_name, preference))
     return prefs_valid
 
 
 def configure_prefs():
-    """Configures prefs for use"""   
+    """Configures prefs for use"""
     _prefs = {}
     keysAndPrompts = [
-        ('UpdatesRootDir', 
+        ('UpdatesRootDir',
          'Filesystem path to store replicated catalogs and updates'),
-        ('UpdatesMetadataDir', 
+        ('UpdatesMetadataDir',
          'Filesystem path to store Reposado metadata'),
-        ('LocalCatalogURLBase', 
-         'Base URL for your local Software Update Service\n(Example: http://su.your.org -- leave empty if you are not replicating updates)'),
+        ('LocalCatalogURLBase',
+         'Base URL for your local Software Update Service\n(Example: '
+         'http://su.your.org -- leave empty if you are not replicating '
+         'updates)'),
         ]
     if not os.path.exists(pref('CurlPath')):
         keysAndPrompts.append(
@@ -216,16 +230,16 @@ def writeDataToPlist(data, filename):
             os.makedirs(metadata_dir)
         except OSError, errmsg:
             print_stderr(
-                'Could not create missing %s because %s', 
+                'Could not create missing %s because %s',
                 metadata_dir, errmsg)
     try:
-        plistlib.writePlist(data, 
+        plistlib.writePlist(data,
             os.path.join(metadata_dir, filename))
     except (IOError, OSError, TypeError), errmsg:
         print_stderr(
             'Could not write %s because %s', filename, errmsg)
-        
-        
+
+
 def getDataFromPlist(filename):
     '''Reads data from a plist in our metadata dir'''
     metadata_dir = pref('UpdatesMetadataDir')
@@ -259,13 +273,13 @@ def writeCatalogBranches(catalog_branches):
 def getProductInfo():
     '''Reads Software Update product info from disk'''
     return getDataFromPlist('ProductInfo.plist')
-    
-    
+
+
 def writeProductInfo(product_info_dict):
     '''Writes Software Update product info to disk'''
     writeDataToPlist(product_info_dict, 'ProductInfo.plist')
-        
-        
+
+
 def getFilenameFromURL(url):
     '''Gets the filename from a URL'''
     (unused_scheme, unused_netloc,
@@ -281,7 +295,7 @@ def getLocalPathNameFromURL(url, root_dir=None):
         path, unused_query, unused_fragment) = urlparse.urlsplit(url)
     relative_path = path.lstrip('/')
     return os.path.join(root_dir, relative_path)
-    
+
 
 def rewriteOneURL(full_url):
     '''Rewrites a single URL to point to our local replica'''
@@ -293,7 +307,7 @@ def rewriteOneURL(full_url):
         return our_base_url + path
     else:
         return full_url
-        
+
 
 def rewriteURLsForProduct(product):
     '''Rewrites the URLs for a product'''
@@ -307,7 +321,7 @@ def rewriteURLsForProduct(product):
             package['MetadataURL'] = rewriteOneURL(
                 package['MetadataURL'])
         # workaround for 10.8.2 issue where client ignores local pkg
-        # and prefers Apple's URL. Need to revisit as we better understand this 
+        # and prefers Apple's URL. Need to revisit as we better understand this
         # issue
         if 'Digest' in package:
             # removing the Digest causes 10.8.2 to use the replica's URL
@@ -343,7 +357,7 @@ def writeBranchCatalogs(localcatalogpath):
     catalog = plistlib.readPlist(localcatalogpath)
     downloaded_products = catalog['Products']
     product_info = getProductInfo()
-    
+
     localcatalogname = os.path.basename(localcatalogpath)
     # now strip the '.sucatalog' bit from the name
     # so we can use it to construct our branch catalog names
@@ -366,7 +380,7 @@ def writeBranchCatalogs(localcatalogpath):
                 catalog['Products'][product_key] = \
                     downloaded_products[product_key]
             elif pref('LocalCatalogURLBase') and product_key in product_info:
-                # Product has probably been deprecated by Apple, 
+                # Product has probably been deprecated by Apple,
                 # so we're using cached product info
                 # First check to see if this product was ever in this
                 # catalog
@@ -394,7 +408,7 @@ def writeBranchCatalogs(localcatalogpath):
                     print_stderr(
                         'WARNING: Product %s not added to branch %s of %s. '
                         'It is not in the corresponding Apple catalogs '
-                        'and is not in the ProductInfo cache.', 
+                        'and is not in the ProductInfo cache.',
                         product_key, branch, localcatalogname)
                 else:
                     print_stderr(
@@ -424,7 +438,7 @@ def writeLocalCatalogs(applecatalogpath):
         localcatalogpath = applecatalogpath[0:-6]
     else:
         localcatalogpath = applecatalogpath
-    
+
     print_stdout('Building %s...' % os.path.basename(localcatalogpath))
     catalog['_CatalogName'] = os.path.basename(localcatalogpath)
     downloaded_products_list = getDownloadStatus()
