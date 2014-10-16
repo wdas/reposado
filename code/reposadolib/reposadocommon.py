05efade8b325ddfbf4cc7a4f4fbbfa282b9dd7af
Greg Neagle <gregneagle@mac.com>
2014-10-16 12:45:50 -0700

    Update docs and default preferences for Yosemite.

--- code/reposadolib/reposadocommon.py
+++ code/reposadolib/reposadocommon.py
@@ -80,7 +80,11 @@ def pref(prefname):
             ('http://swscan.apple.com/content/catalogs/others/'
              'index-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'),
             ('http://swscan.apple.com/content/catalogs/others/'
-          'index-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog')
+             'index-10.9-mountainlion-lion-snowleopard-leopard.merged-1'
+             '.sucatalog'),
+            ('http://swscan.apple.com/content/catalogs/others/'
+             'index-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1'
+             '.sucatalog')
         ],
         'PreferredLocalizations': ['English', 'en'],
         'CurlPath': '/usr/bin/curl'
