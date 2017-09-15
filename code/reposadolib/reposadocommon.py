fc994fba7be758d1adb88921df109b2c65104663
Greg Neagle <gregneagle@mac.com>
2017-09-15 09:47:04 -0700

    Add 10.13 sucatalog URL

--- code/reposadolib/reposadocommon.py
+++ code/reposadolib/reposadocommon.py
@@ -91,7 +91,10 @@ def pref(prefname):
              '.merged-1.sucatalog'),
             ('https://swscan.apple.com/content/catalogs/others/'
              'index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-'
-             'leopard.merged-1.sucatalog')
+             'leopard.merged-1.sucatalog'),
+            ('https://swscan.apple.com/content/catalogs/others/'
+             'index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard-'
+             'leopard.merged-1.sucatalog'),
         ],
         'PreferredLocalizations': ['English', 'en'],
         'CurlPath': '/usr/bin/curl'
