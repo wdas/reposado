b6ae63bd322f684a419f56de6d79770bf0eaa7d9
Greg Neagle <gregneagle@mac.com>
2019-06-04 09:26:46 -0700

    Add macOS Catalina catalog

--- code/reposadolib/reposadocommon.py
+++ code/reposadolib/reposadocommon.py
@@ -98,6 +98,9 @@ def pref(prefname):
             ('https://swscan.apple.com/content/catalogs/others/'
              'index-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-'
              'snowleopard-leopard.merged-1.sucatalog'),
+            ('https://swscan.apple.com/content/catalogs/others/'
+             'index-10.15-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-'
+             'snowleopard-leopard.merged-1.sucatalog'),
         ],
         'PreferredLocalizations': ['English', 'en'],
         'CurlPath': '/usr/bin/curl'
