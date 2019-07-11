6cad7ad3ced245e1b81cbd432b1b08227adeac4a
Greg Neagle <gregneagle@mac.com>
2019-07-11 16:12:28 -0400

    PyLint cleanups

--- code/reposadolib/reposadocommon.py
+++ code/reposadolib/reposadocommon.py
@@ -59,10 +59,10 @@ except ImportError:
 # wrapper for raw_input in Python 3
 try:
     # Python 2
-    get_input = raw_input # pylint: disable=raw_input-builtin
+    get_input = raw_input # pylint: disable=raw_input-builtin,invalid-name
 except NameError:
     # Python 3
-    get_input = input # pylint: disable=input-builtin
+    get_input = input # pylint: disable=input-builtin,invalid-name
 
 def get_main_dir():
     '''Returns the directory name of the script or the directory name of the exe
@@ -70,11 +70,11 @@ def get_main_dir():
     Code from http://www.py2exe.org/index.cgi/HowToDetermineIfRunningFromExe
     '''
     if (hasattr(sys, "frozen") or hasattr(sys, "importers")
-        or imp.is_frozen("__main__")):
+            or imp.is_frozen("__main__")):
         return os.path.dirname(sys.executable)
     return os.path.dirname(sys.argv[0])
 
-def prefsFilePath():
+def prefs_file_path():
     '''Returns path to our preferences file.'''
     return os.path.join(get_main_dir(), 'preferences.plist')
 
@@ -121,26 +121,26 @@ def pref(prefname):
         'CurlPath': '/usr/bin/curl'
     }
     try:
-        prefs = plistlib.readPlist(prefsFilePath())
+        prefs = plistlib.readPlist(prefs_file_path())
     except (IOError, ExpatError):
         prefs = default_prefs
     if prefname in prefs:
         return prefs[prefname]
     elif prefname in default_prefs:
         return default_prefs[prefname]
-    else:
-        return None
+    return None
 
 
-def validPreferences():
+def valid_preferences():
     '''Validates our preferences to make sure needed values are defined
     and paths exist. Returns boolean.'''
     prefs_valid = True
-    for pref_name in ['UpdatesRootDir',  'UpdatesMetadataDir']:
+    for pref_name in ['UpdatesRootDir', 'UpdatesMetadataDir']:
         preference = pref(pref_name)
         if not preference:
-            print_stderr('ERROR: %s is not defined in %s.' %
-                            (pref_name, prefsFilePath()))
+            print_stderr(
+                'ERROR: %s is not defined in %s.' % (pref_name, prefs_file_path())
+            )
             prefs_valid = False
         elif not os.path.exists(preference):
             print_stderr('WARNING: %s "%s" does not exist.'
@@ -152,7 +152,7 @@ def validPreferences():
 def configure_prefs():
     """Configures prefs for use"""
     _prefs = {}
-    keysAndPrompts = [
+    keys_and_prompts = [
         ('UpdatesRootDir',
          'Filesystem path to store replicated catalogs and updates'),
         ('UpdatesMetadataDir',
@@ -163,21 +163,22 @@ def configure_prefs():
          'updates)'),
         ]
     if not os.path.exists(pref('CurlPath')):
-        keysAndPrompts.append(
-        ('CurlPath', 'Path to curl tool (Example: /usr/bin/curl)'))
+        keys_and_prompts.append(
+            ('CurlPath', 'Path to curl tool (Example: /usr/bin/curl)')
+        )
 
-    for (key, prompt) in keysAndPrompts:
+    for (key, prompt) in keys_and_prompts:
         newvalue = get_input('%15s [%s]: ' % (prompt, pref(key)))
         _prefs[key] = newvalue or pref(key) or ''
 
-    prefspath = prefsFilePath()
+    prefspath = prefs_file_path()
     # retrieve current preferences
     try:
         prefs = plistlib.readPlist(prefspath)
     except (IOError, ExpatError):
         prefs = {}
     # merge edited preferences
-    for key in _prefs.keys():
+    for key in _prefs:
         prefs[key] = _prefs[key]
     # write preferences to our file
     try:
@@ -186,7 +187,7 @@ def configure_prefs():
         print_stderr('Could not save configuration to %s', prefspath)
     else:
         # check to make sure they're valid
-        unused_value = validPreferences()
+        _ = valid_preferences()
 
 
 def unicode_or_str(something, encoding="UTF-8"):
@@ -195,7 +196,7 @@ def unicode_or_str(something, encoding="UTF-8"):
         # Python 2
         # pylint: disable=unicode-builtin
         if isinstance(something, str):
-            return unicode(something, encoding) 
+            return unicode(something, encoding)
         return unicode(something)
         # pylint: enable=unicode-builtin
     except NameError:
@@ -206,7 +207,8 @@ def unicode_or_str(something, encoding="UTF-8"):
 
 
 def concat_message(msg, *args):
-    """Concatenates a string with any additional arguments; drops unicode."""
+    """Concatenates a string with any additional arguments;
+    coerces to unicode."""
     msg = unicode_or_str(msg)
     if args:
         args = [unicode_or_str(arg) for arg in args]
@@ -214,8 +216,9 @@ def concat_message(msg, *args):
             msg = msg % tuple(args)
         except TypeError:
             warnings.warn(
-                'String format does not match concat args: %s' % (
-                str(sys.exc_info())))
+                'String format does not match concat args: %s'
+                % (str(sys.exc_info()))
+            )
     return msg
 
 
@@ -256,10 +259,10 @@ def print_stderr(msg, *args):
     if LOGFILE:
         log(output)
     else:
-        print(concat_message(msg, *args), file=sys.stderr)
+        print(output, file=sys.stderr)
 
 
-def humanReadable(size_in_bytes):
+def human_readable(size_in_bytes):
     """Returns sizes in human-readable units."""
     # pylint: disable=round-builtin,old-division
     try:
@@ -274,7 +277,7 @@ def humanReadable(size_in_bytes):
             return str(round(size_in_bytes/float(limit/2**10), 1)) + suffix
 
 
-def writeDataToPlist(data, filename):
+def write_data_to_plist(data, filename):
     '''Writes a dict or list to a plist in our metadata dir'''
     metadata_dir = pref('UpdatesMetadataDir')
     if not os.path.exists(metadata_dir):
@@ -285,14 +288,14 @@ def writeDataToPlist(data, filename):
                 'Could not create missing %s because %s',
                 metadata_dir, errmsg)
     try:
-        plistlib.writePlist(data,
-            os.path.join(metadata_dir, filename))
+        plistlib.writePlist(
+            data, os.path.join(metadata_dir, filename))
     except (IOError, OSError, TypeError) as errmsg:
         print_stderr(
             'Could not write %s because %s', filename, errmsg)
 
 
-def getDataFromPlist(filename):
+def get_data_from_plist(filename):
     '''Reads data from a plist in our metadata dir'''
     metadata_dir = pref('UpdatesMetadataDir')
     try:
@@ -302,54 +305,54 @@ def getDataFromPlist(filename):
         return {}
 
 
-def getDownloadStatus():
+def get_download_status():
     '''Reads download status info from disk'''
-    return getDataFromPlist('DownloadStatus.plist')
+    return get_data_from_plist('DownloadStatus.plist')
 
 
-def writeDownloadStatus(download_status_list):
+def write_download_status(download_status_list):
     '''Writes download status info to disk'''
-    writeDataToPlist(download_status_list, 'DownloadStatus.plist')
+    write_data_to_plist(download_status_list, 'DownloadStatus.plist')
 
 
-def getCatalogBranches():
+def get_catalog_branches():
     '''Reads catalog branches info from disk'''
-    return getDataFromPlist('CatalogBranches.plist')
+    return get_data_from_plist('CatalogBranches.plist')
 
 
-def writeCatalogBranches(catalog_branches):
+def write_catalog_branches(catalog_branches):
     '''Writes catalog branches info to disk'''
-    writeDataToPlist(catalog_branches, 'CatalogBranches.plist')
+    write_data_to_plist(catalog_branches, 'CatalogBranches.plist')
 
 
-def getProductInfo():
+def get_product_info():
     '''Reads Software Update product info from disk'''
-    return getDataFromPlist('ProductInfo.plist')
+    return get_data_from_plist('ProductInfo.plist')
 
 
-def writeProductInfo(product_info_dict):
+def write_product_info(product_info_dict):
     '''Writes Software Update product info to disk'''
-    writeDataToPlist(product_info_dict, 'ProductInfo.plist')
+    write_data_to_plist(product_info_dict, 'ProductInfo.plist')
 
 
-def getFilenameFromURL(url):
+def get_filename_from_url(url):
     '''Gets the filename from a URL'''
     (unused_scheme, unused_netloc,
-        path, unused_query, unused_fragment) = urlsplit(url)
+     path, unused_query, unused_fragment) = urlsplit(url)
     return os.path.basename(path)
 
 
-def getLocalPathNameFromURL(url, root_dir=None):
+def get_local_pathname_from_url(url, root_dir=None):
     '''Derives the appropriate local path name based on the URL'''
     if root_dir is None:
         root_dir = pref('UpdatesRootDir')
     (unused_scheme, unused_netloc,
-        path, unused_query, unused_fragment) = urlsplit(url)
+     path, unused_query, unused_fragment) = urlsplit(url)
     relative_path = path.lstrip('/')
     return os.path.join(root_dir, relative_path)
 
 
-def rewriteOneURL(full_url):
+def rewrite_one_url(full_url):
     '''Rewrites a single URL to point to our local replica'''
     our_base_url = pref('LocalCatalogURLBase')
     if not full_url.startswith(our_base_url):
@@ -357,23 +360,22 @@ def rewriteOneURL(full_url):
         (unused_scheme, unused_netloc,
          path, unused_query, unused_fragment) = urlsplit(full_url)
         return our_base_url + path
-    else:
-        return full_url
+    return full_url
 
 
-def rewriteURLsForProduct(product):
+def rewrite_urls_for_product(product):
     '''Rewrites the URLs for a product'''
     if 'ServerMetadataURL' in product:
-        product['ServerMetadataURL'] = rewriteOneURL(
+        product['ServerMetadataURL'] = rewrite_one_url(
             product['ServerMetadataURL'])
     for package in product.get('Packages', []):
         if 'URL' in package:
-            package['URL'] = rewriteOneURL(package['URL'])
+            package['URL'] = rewrite_one_url(package['URL'])
         if 'MetadataURL' in package:
-            package['MetadataURL'] = rewriteOneURL(
+            package['MetadataURL'] = rewrite_one_url(
                 package['MetadataURL'])
         if 'IntegrityDataURL' in package:
-            package['IntegrityDataURL'] = rewriteOneURL(
+            package['IntegrityDataURL'] = rewrite_one_url(
                 package['IntegrityDataURL'])
         # workaround for 10.8.2 issue where client ignores local pkg
         # and prefers Apple's URL. Need to revisit as we better understand this
@@ -384,11 +386,11 @@ def rewriteURLsForProduct(product):
             del package['Digest']
     distributions = product['Distributions']
     for dist_lang in list(distributions.keys()):
-        distributions[dist_lang] = rewriteOneURL(
+        distributions[dist_lang] = rewrite_one_url(
             distributions[dist_lang])
 
 
-def rewriteURLs(catalog):
+def rewrite_urls(catalog):
     '''Rewrites all the URLs in the given catalog to point to our local
     replica'''
     if pref('LocalCatalogURLBase') is None:
@@ -397,26 +399,26 @@ def rewriteURLs(catalog):
         product_keys = list(catalog['Products'].keys())
         for product_key in product_keys:
             product = catalog['Products'][product_key]
-            rewriteURLsForProduct(product)
+            rewrite_urls_for_product(product)
 
 
-def writeAllBranchCatalogs():
+def write_all_branch_catalogs():
     '''Writes out all branch catalogs. Used when we edit branches.'''
-    for catalog_URL in pref('AppleCatalogURLs'):
-        localcatalogpath = getLocalPathNameFromURL(catalog_URL)
+    for catalog_url in pref('AppleCatalogURLs'):
+        localcatalogpath = get_local_pathname_from_url(catalog_url)
         if os.path.exists(localcatalogpath):
-            writeBranchCatalogs(localcatalogpath)
+            write_branch_catalogs(localcatalogpath)
         else:
             print_stderr(
                 'WARNING: %s does not exist. Perhaps you need to run repo_sync?'
                 % localcatalogpath)
 
 
-def writeBranchCatalogs(localcatalogpath):
+def write_branch_catalogs(localcatalogpath):
     '''Writes our branch catalogs'''
     catalog = plistlib.readPlist(localcatalogpath)
     downloaded_products = catalog['Products']
-    product_info = getProductInfo()
+    product_info = get_product_info()
 
     localcatalogname = os.path.basename(localcatalogpath)
     # now strip the '.sucatalog' bit from the name
@@ -425,7 +427,7 @@ def writeBranchCatalogs(localcatalogpath):
         localcatalogpath = localcatalogpath[0:-10]
 
     # now write filtered catalogs (branches)
-    catalog_branches = getCatalogBranches()
+    catalog_branches = get_catalog_branches()
     for branch in catalog_branches.keys():
         branchcatalogpath = localcatalogpath + '_' + branch + '.sucatalog'
         print_stdout('Building %s...' % os.path.basename(branchcatalogpath))
@@ -459,8 +461,9 @@ def writeBranchCatalogs(localcatalogpath):
                                 'WARNING: Product %s (%s-%s) in branch %s '
                                 'has been deprecated. Will use cached info '
                                 'and packages.',
-                                 product_key, title, version, branch)
-                            rewriteURLsForProduct(catalog_entry)
+                                product_key, title, version, branch
+                            )
+                            rewrite_urls_for_product(catalog_entry)
                             catalog['Products'][product_key] = catalog_entry
                             continue
             else:
@@ -473,20 +476,20 @@ def writeBranchCatalogs(localcatalogpath):
         plistlib.writePlist(catalog, branchcatalogpath)
 
 
-def writeAllLocalCatalogs():
+def write_all_local_catalogs():
     '''Writes out all local and branch catalogs. Used when we purge products.'''
-    for catalog_URL in pref('AppleCatalogURLs'):
-        localcatalogpath = getLocalPathNameFromURL(catalog_URL) + '.apple'
+    for catalog_url in pref('AppleCatalogURLs'):
+        localcatalogpath = get_local_pathname_from_url(catalog_url) + '.apple'
         if os.path.exists(localcatalogpath):
-            writeLocalCatalogs(localcatalogpath)
+            write_local_catalogs(localcatalogpath)
 
 
-def writeLocalCatalogs(applecatalogpath):
+def write_local_catalogs(applecatalogpath):
     '''Writes our local catalogs based on the Apple catalog'''
     catalog = plistlib.readPlist(applecatalogpath)
     # rewrite the URLs within the catalog to point to the items on our
     # local server instead of Apple's
-    rewriteURLs(catalog)
+    rewrite_urls(catalog)
     # remove the '.apple' from the end of the localcatalogpath
     if applecatalogpath.endswith('.apple'):
         localcatalogpath = applecatalogpath[0:-6]
@@ -495,7 +498,7 @@ def writeLocalCatalogs(applecatalogpath):
 
     print_stdout('Building %s...' % os.path.basename(localcatalogpath))
     catalog['_CatalogName'] = os.path.basename(localcatalogpath)
-    downloaded_products_list = getDownloadStatus()
+    downloaded_products_list = get_download_status()
 
     downloaded_products = {}
     product_keys = list(catalog['Products'].keys())
@@ -506,8 +509,8 @@ def writeLocalCatalogs(applecatalogpath):
                 catalog['Products'][product_key]
         else:
             print_stderr('WARNING: did not add product %s to '
-                'catalog %s because it has not been downloaded.',
-                product_key, os.path.basename(applecatalogpath))
+                         'catalog %s because it has not been downloaded.',
+                         product_key, os.path.basename(applecatalogpath))
     catalog['Products'] = downloaded_products
 
     # write raw (unstable/development) catalog
@@ -515,10 +518,10 @@ def writeLocalCatalogs(applecatalogpath):
     plistlib.writePlist(catalog, localcatalogpath)
 
     # now write filtered catalogs (branches) based on this catalog
-    writeBranchCatalogs(localcatalogpath)
+    write_branch_catalogs(localcatalogpath)
 
 
-def readXMLfile(filename):
+def read_xml_file(filename):
     '''Return dom from XML file or None'''
     try:
         dom = minidom.parse(filename)
@@ -533,7 +536,7 @@ def readXMLfile(filename):
     return dom
 
 
-def writeXMLtoFile(node, path):
+def write_xml_to_file(node, path):
     '''Write XML dom node to file'''
     xml_string = node.toxml('utf-8')
     try:
@@ -544,12 +547,12 @@ def writeXMLtoFile(node, path):
         print_stderr('Couldn\'t write XML to %s' % path)
 
 
-def remove_config_data_attribute(product_list):
+def remove_config_data_attr(product_list):
     '''Wrapper to emulate previous behavior of remove-only only operation.'''
-    check_or_remove_config_data_attribute(product_list, remove_attr=True)
+    check_or_remove_config_data_attr(product_list, remove_attr=True)
 
 
-def check_or_remove_config_data_attribute(
+def check_or_remove_config_data_attr( # pylint: disable=invalid-name
         product_list, remove_attr=False, products=None, suppress_output=False):
     '''Loop through the type="config-data" attributes from the distribution
     options for a list of products. Return a list of products that have
@@ -560,7 +563,7 @@ def check_or_remove_config_data_attribute(
     XProtectPlistConfigData and Gatekeeper Configuration Data, which it
     normally does not.'''
     if not products:
-        products = getProductInfo()
+        products = get_product_info()
     config_data_products = set()
     for key in product_list:
         if key in products:
@@ -568,11 +571,11 @@ def check_or_remove_config_data_attribute(
                 distributions = products[key]['CatalogEntry'].get(
                     'Distributions', {})
                 for lang in distributions.keys():
-                    distPath = getLocalPathNameFromURL(
+                    dist_path = get_local_pathname_from_url(
                         products[key]['CatalogEntry']['Distributions'][lang])
-                    if not os.path.exists(distPath):
+                    if not os.path.exists(dist_path):
                         continue
-                    dom = readXMLfile(distPath)
+                    dom = read_xml_file(dist_path)
                     if dom:
                         found_config_data = False
                         option_elements = (
@@ -588,15 +591,15 @@ def check_or_remove_config_data_attribute(
                         # done editing dom
                         if found_config_data and remove_attr:
                             try:
-                                writeXMLtoFile(dom, distPath)
+                                write_xml_to_file(dom, dist_path)
                             except (OSError, IOError):
                                 pass
                             else:
                                 if not suppress_output:
-                                    print_stdout('Updated dist: %s', distPath)
+                                    print_stdout('Updated dist: %s', dist_path)
                         elif not found_config_data:
                             if not suppress_output:
-                                print_stdout('No config-data in %s', distPath)
+                                print_stdout('No config-data in %s', dist_path)
     return list(config_data_products)
 
 LOGFILE = None
